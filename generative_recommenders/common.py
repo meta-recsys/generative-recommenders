ae607e88ebf0f8954a6ac03223769209bc3d39a8
Dark Knight <generatedunixname89002005232357@meta.com>
2025-12-05 23:06:31 -0800

    Revert D86619052
    Reviewed By: yaoyj11
    
    Differential Revision: D88547939

--- generative_recommenders/common.py
+++ generative_recommenders/common.py
@@ -25,7 +25,6 @@ import torch
 
 # @manual=//triton:triton
 import triton
-from generative_recommenders.ops.utils import is_sm100
 
 # @manual=//triton:triton
 from triton.runtime.autotuner import Autotuner
@@ -35,14 +34,6 @@ try:
     torch.ops.load_library("//deeplearning/fbgemm/fbgemm_gpu:sparse_ops_cpu")
 except OSError:
     pass
-
-try:
-    # @manual=//triton:triton
-    import triton.language.extra.tlx  # type: ignore
-
-    HAS_TLX = True
-except ImportError:
-    HAS_TLX = False
 
 try:
     from generative_recommenders.fb.triton_cc.utils import triton_cc
@@ -244,11 +235,6 @@ gpu_unavailable: Tuple[bool, str] = (
 )
 
 gpu_available: bool = not gpu_unavailable[0]
-
-blackwell_tlx_unavailable: Tuple[bool, str] = (
-    not is_sm100() or not HAS_TLX,
-    "Skip TLX and blackwell only tests",
-)
 
 
 def switch_to_contiguous_if_needed(x: torch.Tensor) -> torch.Tensor:
